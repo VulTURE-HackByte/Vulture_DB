e78209d6e3679992d8e7eae580a122c53fdd2f62
NightFury742 <eshaan122@gmail.com>
2024-04-07 02:20:16 +0530

    minor fix

--- backend/routes/scanRoutes.js
+++ backend/routes/scanRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
-const{getSpyderScans, getPassiveScans} = require("../controllers/scanControllers");
+const{getSpiderScans, getPassiveScans} = require("../controllers/scanControllers");
 
-router.route("/spyder").post( protect, getSpyderScans)
+router.route("/spyder").post( protect, getSpiderScans)
 router.route("/passive").post(protect, getPassiveScans)
 
 module.exports = router;
